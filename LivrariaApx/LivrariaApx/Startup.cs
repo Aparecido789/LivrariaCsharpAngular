ca51a831fb5fc82df09964cb3e3acde138ea0f09
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2021-12-05 12:19:35 -0300

    Criando conexão com o banco de dados (EntityFramework - InMemory)

--- LivrariaApx/LivrariaApx/Startup.cs
+++ LivrariaApx/LivrariaApx/Startup.cs
@@ -1,7 +1,9 @@
+using LivrariaApx.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace LivrariaApx
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ToDoContext>(opt => opt.UseInMemoryDatabase(databaseName:"ToDoProjects"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
