29d093954a35e352f363d716559aa1b15d6b428b
Aparecido789 <aparecidofranciscosilva10@gmail.com>
2021-12-05 12:12:21 -0300

    Adicionando Model ToDoContext

--- LivrariaApx/LivrariaApx/Models/Produto.cs
+++ LivrariaApx/LivrariaApx/Models/Produto.cs
@@ -7,5 +7,12 @@ namespace LivrariaApx.Models
 {
     public class Produto
     {
+        public string ID { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quant { get; set; }
+        public string Categoria { get; set; }
+        public string Img { get; set; }
+
     }
 }
